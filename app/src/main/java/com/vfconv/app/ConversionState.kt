2191a114f2a6399f07918c7aa0ed5ece13075896
anemia004 <Khorramshahr04@duck.com>
2026-08-19 21:40:35 +0530

    Delete app directory

--- app/src/main/java/com/vfconv/app/ConversionState.kt
+++
@@ -1,8 +0,0 @@
-package com.vfconv.app
-
-sealed class ConversionState {
-    object Idle : ConversionState()
-    object Running : ConversionState()
-    object Success : ConversionState()
-    data class Error(val message: String?) : ConversionState()
-}
